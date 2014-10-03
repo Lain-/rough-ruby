93def310fd2c010917938c17795f539f0d009eb1
Lain- <ryou@outlook.com>
2014-10-03 22:02:38 +0700

    comment indent

--- scripts/primenumber.rb
+++ scripts/primenumber.rb
@@ -3,15 +3,15 @@ puts 'Written in Ruby by Huynh Yen Loc.'.center(50)
 puts "Type 'EXIT' to exit the program.".center(50)
 puts
 
-print "Enter a number: " #If a very large prime number is entered (16 digits and more), it will take a really long time to run.
+print "Enter a number: "            #If a very large prime number is entered (16 digits and more), it will take a really long time to run.
 number = gets.chomp()
 
 while true
     if number == 'EXIT'
         break
     end
-    number = number.to_i #This will convert the input into 'real' number (integer to be exact).
-    x = 2 #x must be resetted.
+    number = number.to_i            #This will convert the input into 'real' number (integer to be exact).
+    x = 2                           #x must be resetted.
     while true
         if number <= 1
             puts "Invalid number or input!"
@@ -29,8 +29,8 @@ while true
             puts
             break
         end
-        x = x + 1  #The position of this line is very important!
-        if x > Math.sqrt(number) #We only need to check for factor equal or smaller than square root of 'number'.
+        x = x + 1                    #The position of this line is very important!
+        if x > Math.sqrt(number)     #We only need to check for factor equal or smaller than square root of 'number'.
             puts "#{number} is a prime number."
             puts
             break
