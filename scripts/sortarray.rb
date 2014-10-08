fdced167325765d499454252befa7aec5996c39e
Lain- <ryou@outlook.com>
2014-10-08 23:08:32 +0700

    minor correction

--- scripts/sortarray.rb
+++ scripts/sortarray.rb
@@ -1,15 +1,15 @@
 puts 'Type-as-much-as-you-want'.center(50)
-puts 'Written in Ruby by Huynh Yen Loc'.center(50)
+puts 'Written in Ruby by Huynh Yen Loc.'.center(50)
 puts 'Enter an empty line to exit the script.'.center(50)
 puts
 
-words = []
+awesome_array = []
 while true
     print 'Your input: '
     input = gets.chomp()
-    words << input
+    awesome_array << input
     if input == ''
-        puts words.sort
+        puts awesome_array.sort
         break
     end
 end
