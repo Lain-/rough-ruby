3833cfc81d6ca3e8d8dd6016237ea0297fe3cb20
Lain- <ryou@outlook.com>
2014-12-10 20:05:51 +0700

    comment.

--- project Euler/problem2.rb
+++ project Euler/problem2.rb
@@ -5,6 +5,10 @@ fib1 = 1
 fib2 = 2
 sum = 2
 
+#Note: Because the Fibonacci sequence start with one odd and one even number (1 and 2), the next
+#number must be an odd number , after that is another odd number, THEN after that is an even number
+#because odd + odd = even. In summary, the pattern is odd, even, odd, odd, even, odd, odd, even,...
+
 until fib1 > 4000000 || fib2 > 4000000
     fib1 = fib1 + fib2
     if fib1 % 2 == 0
@@ -16,4 +20,4 @@ until fib1 > 4000000 || fib2 > 4000000
     end
 end
 
-puts sum
+puts "The sum of all even Fibonacci numbers not greater than 4 million if #{sum}."
